2685ebf2c20b168ff3a04846c973e13b36cb1175
Selma Saltık <selmasaltiik@gmail.com>
2021-04-05 00:34:47 +0300

    Add files via upload
    nuxt fundamentals

---
+++ Nuxt-js-Fundamentals/nuxt-fundamentals/dist/_nuxt/static/1617571275/posts/whereIsIt/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/posts/whereIsIt", {data:[{}],fetch:{},mutations:[]});
---
+++ Nuxt-js-Fundamentals/nuxt-fundamentals/nuxt.config.js
@@ -0,0 +1,52 @@
+export default {
+  // Target: https://go.nuxtjs.dev/config-target
+  target: 'static',
+
+  generate: {
+    routes: [
+      '/posts/balut',
+      '/posts/whereIsIt',
+      '/posts/how'
+    ]
+  },
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'nuxt-fundamentals',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+    '~/assets/style.css'
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+  ],
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+  }
+}
